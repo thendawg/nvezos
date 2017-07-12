aa513fb60d5a3629ae239b4f560455ea45a4de2f
thendawg <npound7110@gmail.com>
2017-07-12 11:19:56 -0500

    Add files via upload

--- update.sh
+++ update.sh
@@ -4,6 +4,12 @@
 /bin/cp -rf /nvupdate/nvezos/installpayload/html/ /var/www/
 # Update scripts
 /bin/cp -rf /nvupdate/nvezos/scripts/ /nvezos/
+# Fix permissions again
+chown -R www-data /nvezos/
+chmod -R 755 /nvezos/
+chown -R www-data /var/www/html/
+chmod -R 755 /var/www/html/
+
 
 echo "Update is now complete, process will exit shortly."
 
