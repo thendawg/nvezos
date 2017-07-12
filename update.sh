1d38a7ec55ad8a669d5d2e680ae51a9d178499a2
thendawg <npound7110@gmail.com>
2017-07-12 11:35:01 -0500

    Add files via upload

--- update.sh
+++ update.sh
@@ -2,14 +2,19 @@
 
 # Move new php/web files into place
 /bin/cp -rf /nvupdate/nvezos/installpayload/html/ /var/www/
+
 # Update scripts
 /bin/cp -rf /nvupdate/nvezos/scripts/network/getinterface.sh /nvezos/scripts/network/
 /bin/cp -rf /nvupdate/nvezos/scripts/gpu/fixgpu.sh /nvezos/scripts/gpu/
+
 # Fix permissions again
 chown -R www-data /nvezos/
 chmod -R 755 /nvezos/
 chown -R www-data /var/www/html/
 chmod -R 755 /var/www/html/
+
+# Cleanup
+rm -rf /nvupdate/
 
 
 echo "Update is now complete, process will exit shortly."
